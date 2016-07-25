fffc12034b3d174c533f71c8dc522e51ab629e7a
Rahul Salvi <rsalvi006@me.com>
2016-07-25 12:27:38 -0700

    fix

--- src/Visualization/NavigationViewer.py
+++ src/Visualization/NavigationViewer.py
@@ -35,6 +35,11 @@ client = pydsm.Client(SERVERID, CLIENTID, True)
 
 def main(stdscr):
     try:
+        global outputs
+        global angular
+        global linear
+        global kill
+
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
         while(True):
--- src/Visualization/SensorViewer.py
+++ src/Visualization/SensorViewer.py
@@ -38,6 +38,10 @@ client = pydsm.Client(SERVERID, CLIENTID, True)
 
 def main(stdscr):
     try:
+        global data
+        global linear
+        global angular
+
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
         curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
