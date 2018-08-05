e6033e6dca4f180a9f363c20a2ae17b8fdd180ed
Rahul Salvi <rsalvi006@me.com>
2018-08-04 22:27:13 -0700

    make pasb easier to work with and add stages

--- src/Master/PointAndShootBuoys.py
+++ src/Master/PointAndShootBuoys.py
@@ -1,6 +1,7 @@
+# pylint: disable=C0301
 import time
 import sys
-import math
+import pydsm
 from bitstring import BitArray
 sys.path.append("../Dependencies/PythonSharedBuffers/src")
 sys.path.append("../../../DistributedSharedMemory")
@@ -12,32 +13,69 @@ from Serialization import *
 from Constants import *
 from Vision import *
 
-import pydsm
+
+# Point and shoot (+Buoys) follows the following logic:
+#     start:
+#         wait for depth below point - user wants to lock heading
+#         lock heading
+#         set control buffer for depth and heading
+#         wait for unkill - robot started
+#         wait for robot to settle
+#         set control buffer for forward velocity
+#         begin stage two - set new heading based on old heading
+#         wait for robot to settle
+#         move forward
+#         stop
+#         sink to a lower depth
+#         wait for buoy location inputs from forward vision
+#         stop moving forwards
+#         set heading and depth to center buoy in the image
+#         move forward slowly
+#         wait for kill - run complete
+#         unset control buffer
+#         wait for depth above point - robot reset at the surface
+#         goto start
 
 DEPTH_THRESH_ABOVE = 0.25 # meters
 DEPTH_THRESH_BELOW = 0.35 # meters
-TARGET_DEPTH = 2.0 # meters
-FORWARD_SLOW_VEL = 50 # raw
-SETTLING_TIME = 8 # seconds
-ROTATE_CHANGE_AMOUNT = 5 # degrees
-DEPTH_CHANGE_AMOUNT = 0.1 # meters
-BUOY_SETTLING_TIME = 1 # seconds
+COUNT_THRESH       = 5 # counts
 
+# Stage 1 - dead reckon through gate
+
+S1_TARGET_DEPTH  = 1 # meters
+S1_SETTLING_TIME = 8 # seconds
 # Full Run
-FORWARD_VEL = 100 # raw
-COUNT_THRESH = 5 # counts
-GATE_TIME = 25 # seconds
-
+S1_FORWARD_VEL   = 100 # raw
+S1_TRAVEL_TIME   = 25 # seconds
 # Just Buoy Test
-# FORWARD_VEL = 0 # raw
-# COUNT_THRESH = 1 # counts
-# GATE_TIME = 0 # seconds
+# S1_FORWARD_VEL = 0 # raw
+# S1_TRAVEL_TIME = 0 # seconds
+
+# Stage 2 - dead reckon in a different heading
+
+S2_TARGET_DEPTH   = 1 # meters
+S2_SETTLING_TIME  = 8 # seconds
+S2_FORWARD_VEL    = 100 # raw
+S2_TRAVEL_TIME    = 0 # seconds
+S2_HEADING_CHANGE = 0 # degrees
+
+# Stage 3 - prepare for buoys
+
+S3_TARGET_DEPTH   = 2.0 # meters
+S3_SETTLING_TIME  = 8 # seconds
+
+# Stage 4 - attempt buoys
+
+S4_FORWARD_VEL    = 50 # raw
+S4_HEADING_CHANGE = 5 # degrees
+S4_DEPTH_CHANGE   = 0.1 # meters
+S4_LOOP_DELAY     = 0.1 # seconds
 
 XAXIS = 0
 YAXIS = 1
 ZAXIS = 2
 
-SERVER_ID = MASTER_SERVER_ID
+SERVER_ID = 42
 CLIENT_ID = 97
 
 SENSOR_IP = "10.0.0.43"
@@ -182,104 +220,116 @@ def isRobotKilled():
     else:
         return True
 
-# Point and shoot (+Buoys) follows the following logic:
-#     start:
-#         wait for depth below point - user wants to lock heading
-#         lock heading
-#         set control buffer for depth and heading
-#         wait for unkill - robot started
-#         wait for robot to settle
-#         set control buffer for forward velocity
-#         wait for buoy location inputs from forward vision
-#         stop moving forwards
-#         set heading and depth to center buoy in the image
-#         move forward slowly
-#         wait for kill - run complete
-#         unset control buffer
-#         wait for depth above point - robot reset at the surface
-#         goto start
-
 while True:
-    # wait for depth to be below point
-    waitForDepth(DEPTH_THRESH_BELOW, COUNT_THRESH, BELOW)
-    # get heading data from sensor
-    angularData, active = client.getRemoteBufferContents("angular", SENSOR_IP, SENSOR_ID)
-    if active:
-        angular = Unpack(Angular, angularData)
+    try:
+        # wait for depth to be below point
+        waitForDepth(DEPTH_THRESH_BELOW, COUNT_THRESH, BELOW)
+        # get heading data from sensor
+        angularData, active = client.getRemoteBufferContents("angular", SENSOR_IP, SENSOR_ID)
+        if active:
+            angular = Unpack(Angular, angularData)
 
-        # get heading
-        # sensor was rigged to publish euler angles through acc field
-        heading = angular.acc[ZAXIS]
-        print("Following heading: " + str(heading))
+            # get heading
+            # sensor was rigged to publish euler angles through acc field
+            heading = angular.acc[ZAXIS]
+            print("Following heading: " + str(heading))
 
-        # set control buffer
-        controlInput.angular[ZAXIS].pos[0] = heading
-        controlInput.linear[ZAXIS].pos[0] = TARGET_DEPTH
-
-        # write control buffer
-        client.setLocalBufferContents("control", Pack(controlInput))
-
-        # wait for unkill - robot started
-        waitForKill(UNKILLED)
-
-        # wait for robot to settle
-        print("Waiting for robot to settle")
-        time.sleep(SETTLING_TIME)
-
-        # start moving forward
-        print("Moving through gate")
-        controlInput.linear[XAXIS].vel = FORWARD_VEL
-
-        # write control buffer
-        client.setLocalBufferContents("control", Pack(controlInput))
-
-        # pass through gate
-        time.sleep(GATE_TIME)
-        print("Starting buoys")
-
-        # attempt buoys
-        while not isRobotKilled():
-            locationData, loc_active = client.getRemoteBufferContents("targetlocation", FORWARD_IP, FORWARD_ID)
-            angularData, ang_active = client.getRemoteBufferContents("angular", SENSOR_IP, SENSOR_ID)
-            linearData, lin_active = client.getRemoteBufferContents("linear", SENSOR_IP, SENSOR_ID)
-            if loc_active and ang_active and lin_active:
-                location = Unpack(Location, locationData)
-                angular = Unpack(Angular, angularData)
-                linear = Unpack(Linear, linearData)
-                if location.y or location.z:
-                    controlInput.linear[XAXIS].vel = 0
-
-                    controlInput.linear[ZAXIS].pos[0] = controlInput.linear[ZAXIS].pos[0] + (DEPTH_CHANGE_AMOUNT * location.z)
-                    if controlInput.linear[ZAXIS].pos[0] > 3:
-                        controlInput.linear[ZAXIS].pos[0] = 3
-                        if not location.y:
-                            controlInput.linear[XAXIS].vel = FORWARD_SLOW_VEL
-                    if controlInput.linear[ZAXIS].pos[0] < 1.5:
-                        controlInput.linear[ZAXIS].pos[0] = 1.5
-                        if not location.y:
-                            controlInput.linear[XAXIS].vel = FORWARD_SLOW_VEL
-
-                    controlInput.angular[ZAXIS].pos[0] = controlInput.angular[ZAXIS].pos[0] + (ROTATE_CHANGE_AMOUNT * location.y)
-                    while controlInput.angular[ZAXIS].pos[0] > 180:
-                        controlInput.angular[ZAXIS].pos[0] = controlInput.angular[ZAXIS].pos[0] - 360
-                    while controlInput.angular[ZAXIS].pos[0] < -180:
-                        controlInput.angular[ZAXIS].pos[0] = controlInput.angular[ZAXIS].pos[0] + 360
-                else:
-                    controlInput.linear[XAXIS].vel = FORWARD_SLOW_VEL
-            else:
-                controlInput.linear[XAXIS].vel = 0
+            # set control buffer
+            controlInput.angular[ZAXIS].pos[0] = heading
+            controlInput.linear[ZAXIS].pos[0] = S1_TARGET_DEPTH
             client.setLocalBufferContents("control", Pack(controlInput))
-            time.sleep(BUOY_SETTLING_TIME)
 
-        print("Finished run, resetting")
+            # wait for unkill - robot started
+            print("Waiting for unkill")
+            waitForKill(UNKILLED)
 
-        # unset control buffer
-        controlInput.angular[ZAXIS].pos[0] = 0
-        controlInput.linear[XAXIS].vel = 0
-        controlInput.linear[ZAXIS].pos[0] = 0
+            # wait for robot to settle
+            print("Waiting for robot to settle")
+            time.sleep(S1_SETTLING_TIME)
 
-        # write control buffer
-        client.setLocalBufferContents("control", Pack(controlInput))
+            # start moving forward
+            print("Moving through gate")
+            controlInput.linear[XAXIS].vel = S1_FORWARD_VEL
+            client.setLocalBufferContents("control", Pack(controlInput))
+            time.sleep(S1_TRAVEL_TIME)
 
-        # wait for depth to be above point
-        waitForDepth(DEPTH_THRESH_ABOVE, COUNT_THRESH, ABOVE)
+            # start second stage
+            print("Starting second stage")
+            controlInput.linear[XAXIS].vel = 0
+            controlInput.angular[ZAXIS].pos[0] = heading + S2_HEADING_CHANGE
+            controlInput.linear[ZAXIS].pos[0] = S2_TARGET_DEPTH
+            client.setLocalBufferContents("control", Pack(controlInput))
+
+            # wait for robot to settle
+            print("Waiting for robot to settle")
+            time.sleep(S2_SETTLING_TIME)
+
+            # start moving forward
+            print("Moving forward")
+            controlInput.linear[XAXIS].vel = S2_FORWARD_VEL
+            client.setLocalBufferContents("control", Pack(controlInput))
+            time.sleep(S2_TRAVEL_TIME)
+
+            # start third stage
+            print("Starting third stage")
+            controlInput.linear[XAXIS].vel = 0
+            controlInput.linear[ZAXIS].pos[0] = S3_TARGET_DEPTH
+            client.setLocalBufferContents("control", Pack(controlInput))
+
+            # wait for robot to settle
+            print("Waiting for robot to settle")
+            time.sleep(S3_SETTLING_TIME)
+
+            # start fourth stage
+            print("Starting fourth stage (buoys)")
+            prev_counter = 0
+            while not isRobotKilled():
+                locationData, loc_active = client.getRemoteBufferContents("targetlocation", FORWARD_IP, FORWARD_ID)
+                angularData, ang_active = client.getRemoteBufferContents("angular", SENSOR_IP, SENSOR_ID)
+                linearData, lin_active = client.getRemoteBufferContents("linear", SENSOR_IP, SENSOR_ID)
+                if loc_active and ang_active and lin_active:
+                    location = Unpack(Location, locationData)
+                    angular = Unpack(Angular, angularData)
+                    linear = Unpack(Linear, linearData)
+                    if location.loctype != prev_counter:
+                        prev_counter = location.loctype
+                        if location.y or location.z:
+                            controlInput.linear[XAXIS].vel = 0
+
+                            controlInput.linear[ZAXIS].pos[0] = controlInput.linear[ZAXIS].pos[0] + (S4_DEPTH_CHANGE * location.z)
+                            if controlInput.linear[ZAXIS].pos[0] > 3:
+                                controlInput.linear[ZAXIS].pos[0] = 3
+                                if not location.y:
+                                    controlInput.linear[XAXIS].vel = S4_FORWARD_VEL
+                            if controlInput.linear[ZAXIS].pos[0] < 1.5:
+                                controlInput.linear[ZAXIS].pos[0] = 1.5
+                                if not location.y:
+                                    controlInput.linear[XAXIS].vel = S4_FORWARD_VEL
+
+                            controlInput.angular[ZAXIS].pos[0] = controlInput.angular[ZAXIS].pos[0] + (S4_HEADING_CHANGE * location.y)
+                            while controlInput.angular[ZAXIS].pos[0] > 180:
+                                controlInput.angular[ZAXIS].pos[0] = controlInput.angular[ZAXIS].pos[0] - 360
+                            while controlInput.angular[ZAXIS].pos[0] < -180:
+                                controlInput.angular[ZAXIS].pos[0] = controlInput.angular[ZAXIS].pos[0] + 360
+                        else:
+                            controlInput.linear[XAXIS].vel = S4_FORWARD_VEL
+                else:
+                    controlInput.linear[XAXIS].vel = 0
+                client.setLocalBufferContents("control", Pack(controlInput))
+                time.sleep(S4_LOOP_DELAY)
+
+            print("Finished run, resetting")
+
+            # unset control buffer
+            controlInput.angular[ZAXIS].pos[0] = 0
+            controlInput.linear[XAXIS].vel = 0
+            controlInput.linear[ZAXIS].pos[0] = 0
+
+            # write control buffer
+            client.setLocalBufferContents("control", Pack(controlInput))
+
+            # wait for depth to be above point
+            waitForDepth(DEPTH_THRESH_ABOVE, COUNT_THRESH, ABOVE)
+    except KeyboardInterrupt:
+        print("Caught control-C, exiting")
+        break
